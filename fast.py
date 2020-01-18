8a60b290ff407b8ae84f46e634d19fcccb9ebdef
Max Humber <max.humber@gmail.com>
2020-01-18 16:20:21 -0500

    more wip

--- fast.py
+++ fast.py
@@ -1,46 +1,80 @@
-import pandas as pd
+from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
 from sklearn.datasets import load_breast_cancer
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import ParameterGrid
+from sklearn.model_selection import ParameterGrid, train_test_split
 from sklearn.metrics import balanced_accuracy_score
-from matplotlib import pyplot as plt
-
 %matplotlib inline
+
+# data prep
 
 cancer = load_breast_cancer()
 df = pd.DataFrame(cancer.data, columns=cancer.feature_names)
 df.columns = [c.replace(' ', '_') for c in df.columns]
 df['target'] = cancer.target
-df['target'] = df.target.replace({0: 'malignant', 1: 'benign'})
+df['target'] = df.target.replace({0: 1, 1: 0}) # reverse
+df.to_csv('cancer.csv', index=False)
+df = pd.read_csv('cancer.csv')
 
-# separation
+# train test split
 
 target = 'target'
 X = df.drop(target, axis=1)
 y = df[target]
-le = LabelEncoder()
-y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42)
 
-# experiment
+# WIP build
 
 feature = 'worst_concave_points'
 
-p = 50
-threshold = np.percentile(X[feature], 50)
+max_levels = 4
+max_thresholds = 10
 
-feature_cuts = np.where(X[feature] > threshold, 'left', 'right')
+thresholds = np.linspace(0, 100, num=max_thresholds+1)[1:-1]
 
-decision = pd.DataFrame(zip(X[feature], feature_cuts, y), columns=['feature', 'cut', 'y'])
-majority = decision.groupby('cut')['y'].mean()
+percentile = thresholds[4]
+threshold = np.percentile(X[feature], percentile)
+directions = np.where(X[feature] <= threshold, 'left', 'right')
 
-# BUG: maybe could be the same, or maybe doesn't get rounded?
-left = round(majority['left'])
-right = round(majority['right'])
+node = pd.DataFrame(zip(X[feature], directions, y), columns=['feature', 'direction', 'y'])
+majority = node.groupby('direction')['y'].mean()
 
-decision['yhat'] = decision['cut'].replace({'left': left, 'right': right})
+if majority['left'] <= majority['right']:
+    left, right = 0, 1
+else:
+    left, right = 1, 0
 
-bacc = balanced_accuracy_score(decision['y'], decision['yhat'])
+node['yhat'] = node['direction'].replace({'left': left, 'right': right})
+
+metric = balanced_accuracy_score
+
+metric(node['y'], node['yhat'])
+
+node
+
+### decide which direction to split next
+
+from itertools import combinations
+
+def gini(x):
+    n = len(x)
+    diffs = sum(abs(i - j) for i, j in combinations(x, r=2))
+    return diffs / (2 * n**2 * x.mean())
+
+gini_left = gini(node[node['direction'] == 'left']['y'])
+gini_right = gini(node[node['direction'] == 'right']['y'])
+
+if gini_left >= gini_right:
+    next_split_direction = 'left'
+else:
+    next_split_direction = 'right'
+
+X[node['direction'] == next_split_direction]
+
+
+
 
 # wrap in a function
 
@@ -79,14 +113,7 @@ bacc
 
 
 
-from typing import List
-from itertools import combinations
 
-def gini(x: List[float]) -> float:
-    x = np.array(x, dtype=np.float32)
-    n = len(x)
-    diffs = sum(abs(i - j) for i, j in combinations(x, r=2))
-    return diffs / (2 * n**2 * x.mean())
 
 X[feature]
 threshold = np.percentile(X[feature], percentile)
